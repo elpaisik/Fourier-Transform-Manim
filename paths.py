1d4f0c721acb9271d6c8fd4772919ca75031341e
George Ogden <george-ogden@outlook.com>
2025-06-09 11:51:43 +0100

    Use updated TSP solver to calculate path

--- paths.py
+++ paths.py
@@ -1,46 +1,17 @@
+from python_tsp.heuristics import solve_tsp_lin_kernighan
 import numpy as np
 
 from tqdm import trange
 import platform
 
-# adapted from https://github.com/diego-vicente/som-tsp
-def self_organising_maps(points: np.ndarray, iterations: int = 0, learning_rate: float = 0.8) -> np.ndarray:
-    # keep points only once
-    # and then the population size is 8 times the number of cities
+def self_organising_maps(points: np.ndarray) -> np.ndarray:
+    # use known solver
     points = np.unique(points)
-    n = len(points) * 8
+    distance_matrix = np.abs(points[:,None] - points[None,:])
 
-    # generate an adequate network of neurons
-    network = np.random.uniform(min(points.real), max(
-        points.real), n) + 1j * np.random.uniform(min(points.imag), max(points.imag), n)
+    optimal_solution, _ = solve_tsp_lin_kernighan(distance_matrix)
 
-    if iterations == 0:
-        iterations = min(100000, int(np.log(n) / -np.log(.9997)))
-
-    for _ in trange(iterations, desc="Optimising shape", ascii=True if platform.system() == "Windows" else None, leave=False):
-        # choose a random city
-        point = np.random.choice(points)
-        idx = abs(network - point).argmin()
-
-        # generate a filter that applies changes to the winner's gaussian
-        # compute the circular network distance to the center
-        deltas = np.absolute(idx - np.arange(len(network)))
-        distances = np.minimum(deltas, len(network) - deltas)
-        # impose an upper bound on the radix to prevent NaN and blocks
-        radix = max(n / 10, 1)
-        # compute Gaussian distribution around the given center
-        gaussian = np.exp(-(distances**2) / (2*radix**2))
-
-        # update the network's weights (closer to the city)
-        network += gaussian * learning_rate * (point - network)
-
-        # decay the variables
-        learning_rate *= 0.99997
-        n *= 0.9997
-
-    # find route from network
-    route = points[np.argsort(
-        [np.argmin(abs(network - point)) for point in points])]
+    route = points[optimal_solution]
     return route
 
 
